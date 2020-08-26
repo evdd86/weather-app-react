4006e498f11c4b484c7dcdbee5cafa2b9358ebf1
evdd86 <evdd86@hotmail.com>
2020-08-26 20:47:34 +0200

    Add defaultUnit + footer

--- src/App.js
+++ src/App.js
@@ -5,9 +5,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="container border border-dark">
-        <SearchForm defaultCity="Brussels" />
+      <div className="container">
+        <SearchForm defaultCity="Brussels" defaultUnit="°C" />
       </div>
+      <footer>
+        {" "}
+        <a href="https://github.com/evdd86/weather-app-react">
+          Open sourced code
+        </a>{" "}
+        by Ellen Van der Donckt <br />
+        <a href="https://www.iconfinder.com/iconsets/sketchy-weather-icons-by-azuresol">
+          Sketchy Weather Icons by Azuresol @ Iconfinder
+        </a>{" "}
+        <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.en">
+          (licence)
+        </a>
+      </footer>
     </div>
   );
 }
